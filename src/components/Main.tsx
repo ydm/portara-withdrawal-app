72d1b2983f66ae372dd76eec0b85be4e556fac93
Yordan Miladinov <jordanmiladinov@gmail.com>
2023-03-21 03:34:23 +0200

    clean up #2

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { Web3Button } from "@web3modal/react";
 import { useAccount } from "wagmi";
-import { BigNumber } from "ethers";
 
 type Status = "connected" | "reconnecting" | "connecting" | "disconnected";
 
